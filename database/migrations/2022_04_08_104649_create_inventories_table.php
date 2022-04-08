784758cc53d196b7080944ad89311c7e2f7bdc84
Dcrown97 <aisimiyuoluwadara@gmail.com>
2022-04-08 12:18:46 +0100

    commit

---
+++ database/migrations/2022_04_08_104649_create_inventories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('item');
+            $table->string('colour');
+            $table->string('area');
+            $table->string('content');
+            $table->string('category');
+            $table->string('status');
+            $table->string('date_found');
+            $table->string('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
